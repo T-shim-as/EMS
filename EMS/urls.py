d1c8e0b9ff92f2b7fb06cb6f07d8723c574674a7
T-shim-as <holsoc@gmail.com>
2024-10-18 07:15:36 +0900

    rog

--- EMS/urls.py
+++ EMS/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
+    path('project/', include('projects.urls')),
     # path('', TemplateView.as_view(template_name='home.html'), name='home'),
     # path('accounts/', include('accounts.urls')),
     # path('projects/', include('projects.urls')),
--- projects/models.py
+++ projects/models.py
@@ -1,19 +1,38 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Project(models.Model):
     name = models.CharField(max_length=200)  # プロジェクト名
     description = models.TextField()  # プロジェクトの説明
     start_date = models.DateField()  # プロジェクト開始日
+    must_skill = models.CharField(max_length=200, null=True, blank=True)  # 必須スキル（オプション） 
+    better_skill = models.CharField(max_length=200, null=True, blank=True)  # 尚可スキル（オプション） 
     end_date = models.DateField(null=True, blank=True)  # プロジェクト終了日（オプション）
-    time = models.TextField(null=True, blank=True)  # 勤務時間（オプション）
-    address = models.TextField(null=True, blank=True)  # 勤務場所
+    time = models.CharField(max_length=200, null=True, blank=True)  # 勤務時間（オプション）
+    address = models.CharField(max_length=200, null=True, blank=True)  # 勤務場所
     status = models.CharField(max_length=20, choices=[  # プロジェクトの状態
         ('proposal', '提案中'),
         ('ongoing', '進行中'),
+        ('interview', '面談'),
         ('waiting', '結果待ち'),
-        ('confirmed', '確定'),
-        ('failure', '失敗'),
+        ('join', '確定'),
     ], default='proposal')
+
+    # 案件合致度（業務内容）
+    content_match = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        null=True,
+        blank=True,
+        verbose_name="業務内容合致度"
+    )
+
+    # 案件合致度（就業環境）
+    environment_match = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        null=True,
+        blank=True,
+        verbose_name="就業環境合致度"
+    )
 
     def __str__(self):
         return self.name
